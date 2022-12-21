baa6f53073b83164521a334d7260734ddee63484
Ekaterina <sweetykiti@gmail.com>
2022-12-21 12:51:05 +0300

    Решение первой задачи

---
+++ Task01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+void PositiveNumber (int []array)
+{   int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if (array[i] > 0) count++;
+    }
+    Console.WriteLine($"[{string.Join(",", array)}]-> {count}");
+}
+
+Console.Write("Введите размерность массива: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите элемент {0} :", i + 1);
+    array[i] = int.Parse(Console.ReadLine());
+}
+PositiveNumber(array);
---
+++ Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+int Data(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+double b1 = Data ("Введите число b1 ");
+double k1 = Data ("Введите число k1 ");
+double b2 = Data ("Введите число b2 ");
+double k2 = Data ("Введите число k2 ");
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+Console.WriteLine ($"b1 = {b1},k1 = {k1},b2 = {b2},k2 = {k2} -> ({x},{y})");
+
